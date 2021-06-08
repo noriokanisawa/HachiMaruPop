efaa0d31f7bcace6cf4255b746911df1243af982
Aaron <aaron@sajatypeworks.com>
2021-06-08 11:07:40 -0700

    Hinting removed
    Per GF request, hinting has been removed and GASP table set to smoothing. No impact on outlines.

--- build.py
+++ build.py
@@ -7,6 +7,13 @@ __main__.main(("-g","sources/HachiMaruPop.glyphs", "-o","ttf",))
 
 path = "master_ttf/HachiMaruPop-Regular.ttf"
 
+def GASP_set(font:TTFont):
+    if "gasp" not in font:
+        font["gasp"] = newTable("gasp")
+        font["gasp"].gaspRange = {}
+    if font["gasp"].gaspRange != {65535: 0x000A}:
+        font["gasp"].gaspRange = {65535: 0x000A}
+
 modifiedFont = TTFont(path)
 print ("Adding additional tables")
 modifiedFont["DSIG"] = newTable("DSIG")     #need that stub dsig
@@ -21,19 +28,10 @@ modifiedFont["head"].flags |= 1 << 3        #sets flag to always round PPEM to i
 modifiedFont["name"].addMultilingualName({'ja':'はちまるポップ'}, modifiedFont, nameID = 1, windows=True, mac=False)
 modifiedFont["name"].addMultilingualName({'ja':'Regular'}, modifiedFont, nameID = 2, windows=True, mac=False)
 
+GASP_set(modifiedFont)
+
 modifiedFont.save("fonts/ttf/HachiMaruPop-Regular.ttf")
 
 shutil.rmtree("instance_ufo")
 shutil.rmtree("master_ufo")
 shutil.rmtree("master_ttf")
-
-subprocess.check_call(
-        [
-            "ttfautohint",
-            "--stem-width",
-            "nsn",
-            "fonts/ttf/HachiMaruPop-Regular.ttf",
-            "fonts/ttf/HachiMaruPop-Regular-hinted.ttf",
-        ]
-    )
-shutil.move("fonts/ttf/HachiMaruPop-Regular-hinted.ttf", "fonts/ttf/HachiMaruPop-Regular.ttf")
